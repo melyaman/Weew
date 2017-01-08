b5a935558392aaeccd94de2b98f07453aa8c8bf8
melyaman <mohamed.elyamani@telecom-bretagne.eu>
2017-01-08 23:03:55 +0100

    added calendar popup (Create fragment)

--- app/src/main/java/preferences/android/eurecom/fr/weew3/Create.java
+++ app/src/main/java/preferences/android/eurecom/fr/weew3/Create.java
@@ -1,14 +1,23 @@
 package preferences.android.eurecom.fr.weew3;
 
 
+import android.app.DatePickerDialog;
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
+
+import java.util.Locale;
 
 
 /**
@@ -22,21 +31,56 @@ public class Create extends Fragment {
     }
 
 
-
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         // Inflate the layout for this fragment
-        final View rootView =  inflater.inflate(R.layout.fragment_create, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_create, container, false);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(rootView.getContext(),
                 R.array.event_choices, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
+        // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
+        // Apply the adapter to the spinner
         Spinner spinner = (Spinner) rootView.findViewById(R.id.eventTypeSpinner);
         spinner.setAdapter(adapter);
+        ///// calendar popup ////
+        final EditText edittext;
+        edittext = (EditText) rootView.findViewById(R.id.eventDateText);
+        final Calendar myCalendar = Calendar.getInstance();
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+
+            private void updateLabel() {
+
+                String myFormat = "MM/dd/yyyy"; //In which you need put here
+                SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+                edittext.setText(sdf.format(myCalendar.getTime()));
+
+            }
+        };
+        edittext.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(rootView.getContext(), date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+
+            }
+
+        });
         return rootView;
     }
-
 }
