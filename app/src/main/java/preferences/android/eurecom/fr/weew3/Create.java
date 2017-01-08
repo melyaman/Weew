7ef35ce6ce9953d5a6915034071f4a8e220d930d
melyaman <mohamed.elyamani@telecom-bretagne.eu>
2017-01-08 22:23:20 +0100

    added events spinner (Create fragment)

--- app/src/main/java/preferences/android/eurecom/fr/weew3/Create.java
+++ app/src/main/java/preferences/android/eurecom/fr/weew3/Create.java
@@ -2,10 +2,13 @@ package preferences.android.eurecom.fr.weew3;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
 /**
@@ -19,11 +22,21 @@ public class Create extends Fragment {
     }
 
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create, container, false);
+        // Inflate the layout for this fragment
+        final View rootView =  inflater.inflate(R.layout.fragment_create, container, false);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(rootView.getContext(),
+                R.array.event_choices, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        Spinner spinner = (Spinner) rootView.findViewById(R.id.eventTypeSpinner);
+        spinner.setAdapter(adapter);
+        return rootView;
     }
 
 }
--- app/src/main/java/preferences/android/eurecom/fr/weew3/Events.java
+++ app/src/main/java/preferences/android/eurecom/fr/weew3/Events.java
@@ -6,24 +6,33 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Events extends Fragment {
+public class Events extends Fragment  {
 
 
     public Events() {
         // Required empty public constructor
 
     }
+    //Spinner spinner = (Spinner) view.findViewById(R.id.eventTypeSpinner);
+    // Create an ArrayAdapter using the string array and a default spinner layout
 
-
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_events, container, false);
     }
 
