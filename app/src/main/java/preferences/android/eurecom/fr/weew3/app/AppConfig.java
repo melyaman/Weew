9352414a9762018f3313cb8f2c8b2f3b6e289ae0
AhmedAbdelli <ahmedabdelli.sup@gmail.com>
2017-01-10 13:45:23 +0100

    Login, Registration and Session Management

---
+++ app/src/main/java/preferences/android/eurecom/fr/weew3/app/AppConfig.java
@@ -0,0 +1,13 @@
+package preferences.android.eurecom.fr.weew3.app;
+
+/**
+ * Created by aabdelli on 02/01/2017.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://172.24.11.157/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://172.24.11.157/android_login_api/register.php";
+}
