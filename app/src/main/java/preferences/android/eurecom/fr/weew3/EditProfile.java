f2310008781fe989252e7e3cefc5bb4ca8a9aa57
zaouali <hamza.zaouali@eurecom.fr>
2017-01-17 14:35:13 +0100

    adding edit profile

--- app/src/main/java/preferences/android/eurecom/fr/weew3/EditProfile.java
+++ app/src/main/java/preferences/android/eurecom/fr/weew3/EditProfile.java
@@ -1,11 +1,49 @@
 package preferences.android.eurecom.fr.weew3;
 
 
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.icu.util.Calendar;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import preferences.android.eurecom.fr.weew3.app.AppConfig;
+import preferences.android.eurecom.fr.weew3.app.AppController;
+import preferences.android.eurecom.fr.weew3.helper.SQLiteHandler;
+import preferences.android.eurecom.fr.weew3.helper.SessionManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.app.Activity.RESULT_OK;
 
 
 /**
@@ -13,17 +51,142 @@ import android.view.ViewGroup;
  */
 public class EditProfile extends Fragment {
 
+    private static final String TAG = EditProfile.class.getSimpleName();
+    int RESULT_LOAD_IMAGE = 1;
+    final String FILEPATH = "FilePath";
 
-    public EditProfile() {
-        // Required empty public constructor
-    }
+    SharedPreferences prefs;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_profile, container, false);
+       // return inflater.inflate(R.layout.fragment_edit_profile, container, false);
+
+        contextOfApplication = getActivity().getApplicationContext();
+
+        final View rView = inflater.inflate(R.layout.fragment_edit_profile, container, false);
+
+        final EditText new_username;
+        final EditText new_password;
+        final ImageButton myImage;
+        new_username = (EditText) rView.findViewById(R.id.new_username);
+        new_password = (EditText) rView.findViewById(R.id.new_password);
+        myImage = (ImageButton) rView.findViewById(R.id.myButton);
+
+
+/*
+        myImage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Toast.makeText(rView.getContext(),
+                        "ImageButton is clicked!", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+               // intent.setType("image/*");
+                //intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"),1);
+
+            }
+        });
+*/
+
+        /// edit the photo
+
+
+
+
+
+            prefs = this.getActivity().getSharedPreferences("com.example.app", Context.MODE_PRIVATE);
+        myImage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(i, 1);
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+        ////
+
+
+
+
+return rView;
     }
 
+/*
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            switch (requestCode) {
+
+                case 1:
+                    if (resultCode == Activity.RESULT_OK) {
+                        //data gives you the image uri. Try to convert that to bitmap
+                        break;
+                    } else if (resultCode == Activity.RESULT_CANCELED) {
+                        Log.e(TAG, "Selecting picture cancelled");
+                    }
+                    break;
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Exception in onActivityResult : " + e.getMessage());
+        }
+    }
+
+*/
+
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Context applicationContext = this.getContextOfApplication();
+
+            Cursor cursor = applicationContext.getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            prefs.edit().putString(FILEPATH, picturePath);
+
+        }
+
+
+    }
+
+
+
+    public static Context contextOfApplication;
+    public static Context getContextOfApplication()
+    {
+        return contextOfApplication;
+    }
 }
